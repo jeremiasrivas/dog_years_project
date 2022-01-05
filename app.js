b5cab7345974e32514f0e33675676b277774dcc3
jere <jererivas03@gmail.com>
2022-01-05 15:08:48 -0800

    finished 2nd project

---
+++ app.js
@@ -0,0 +1,18 @@
+// set number as my age 
+let myAge = 18
+// set early years
+let earlyYears = 2;
+earlyYears *= 10.5
+// myAge variable and subtracted 2 
+let laterYears = myAge -2;
+//multiply by 4 
+laterYears *= 4
+console.log(earlyYears);
+console.log(laterYears);
+
+//added earlyYears and laterYeears together, and store in a variable named myAgeInDogYears
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = "Jere".toLowerCase();
+//displays your name and age in dog years. 
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`)
